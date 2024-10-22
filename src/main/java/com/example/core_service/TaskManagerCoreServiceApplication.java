5d160dd913d5aac7786107d70174c86e5b73a900
Mikhail Bondarev <bondarevmisha99@gmail.com>
2024-10-22 21:55:20 +0700

    Renamed project

--- src/main/java/com/example/core_service/TaskManagerCoreServiceApplication.java
+++ src/main/java/com/example/core_service/TaskManagerCoreServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreServiceApplication {
+public class TaskManagerCoreServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreServiceApplication.class, args);
+		SpringApplication.run(TaskManagerCoreServiceApplication.class, args);
 	}
 
 }
