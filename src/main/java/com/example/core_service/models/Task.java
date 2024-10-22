66d9a1be4173440e6c3e7b8abe8867835219f53f
Mikhail Bondarev <bondarevmisha99@gmail.com>
2024-10-22 21:38:35 +0700

    Entities created

---
+++ src/main/java/com/example/core_service/models/Task.java
@@ -0,0 +1,39 @@
+package com.example.core_service.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "task_finished")
+    private LocalDateTime taskFinished;
+
+    @JoinColumn
+    @ManyToOne
+    User ownerId;
+}
---
+++ src/main/java/com/example/core_service/models/User.java
@@ -0,0 +1,27 @@
+package com.example.core_service.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", unique = true)
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+}
